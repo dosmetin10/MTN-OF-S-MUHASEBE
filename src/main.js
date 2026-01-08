ca11cd7660b9e2bd6fb4952f853d62d87ddcd29f
MTN <dos.metin10@gmail.com>
2026-01-08 21:44:57 +0300

    Add starter Electron dashboard UI

---
+++ src/main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    backgroundColor: "#f5f7fb",
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js")
+    }
+  });
+
+  mainWindow.loadFile(path.join(__dirname, "renderer", "index.html"));
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
